08be6caf38e537dc89ebc7158eb3bf85757821e7
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-10-11 19:09:01 +0100

    all functionalities complete

--- InvApp/InvApp/settings.py
+++ InvApp/InvApp/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%ka$4gx90_1zlt1=&0q(8uy9pxp3h^@b5*pc4-=x5jp3a)%5&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-dev-secret-key-change-this')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
